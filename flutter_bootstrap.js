2ab4ffe428b1afd95ac9ce0e4634dcfb50bc4d7d
hdyeon67 <hdyeon67@gmail.com>
2024-10-08 21:39:50 +0900

    #6.16Url Launcher

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"059e4e6d8ff6de39c29441c53e949bfb0bf179
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "821536018"
+    serviceWorkerVersion: "932759466"
   }
 });
